d13ee28799c6257a166e04878ff581ed6f827e76
Filip Hracek <filiph@google.com>
2012-04-29 09:50:03 +0200

    Updated the Google Closure library and compiler.

--- scripts/compile-tztime.sh
+++ scripts/compile-tztime.sh
@@ -1,1 +1,1 @@
-../static/js/closure-library/closure/bin/build/closurebuilder.py --root=../static/js --namespace="filiph.TzTime" --output_mode=compiled --compiler_jar=./compiler.jar > ../static/js/tztime-min.js
+../static/js/closure-library-read-only/closure/bin/build/closurebuilder.py --root=../static/js --namespace="filiph.TzTime" --output_mode=compiled --compiler_jar=./compiler.jar > ../static/js/tztime-min.js
